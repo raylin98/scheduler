9f204bd6b391e09843bb30ed99a68890e9cfb84e
Raymond Lin <raymond.lin.7690@gmail.com>
2023-07-15 18:24:55 -0700

    Adding stories for index, Header, Empty and implementing functionality

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "components/Appointment/styles.scss"
+
+import Header from "components/Appointment/Header."
+
+export default function Appointment(props) {
+  return (
+  <article className="appointment">
+    <Header time={props.time}/>
+  </article>
+  );
+}
