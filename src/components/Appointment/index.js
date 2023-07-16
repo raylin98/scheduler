515231941fbf7b4a7399347669d495a385533e13
Raymond Lin <raymond.lin.7690@gmail.com>
2023-07-16 03:20:33 -0700

    Adding stories for appointment empty and booked, refactored show.js to take props and updating appointment index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,10 +4,17 @@ import "components/Appointment/styles.scss"
 
 import Header from "components/Appointment/Header."
 
+import Empty from "./Empty";
+import Show from "./Show";
+
 export default function Appointment(props) {
   return (
   <article className="appointment">
     <Header time={props.time}/>
+    {props.interview ? <Show 
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+    /> : <Empty />}
   </article>
   );
 }
