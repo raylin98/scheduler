e1ce6658484ff33b0ee97c7a50616c1a5cfc0edc
Raymond Lin <raymond.lin.7690@gmail.com>
2023-07-22 03:08:17 -0700

    Rendering empty, show and create form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,15 +6,33 @@ import Header from "components/Appointment/Header."
 
 import Empty from "./Empty";
 import Show from "./Show";
+import Form from "./Form";
+import useVisualMode from "../hooks/useVisualMode";
 
 export default function Appointment(props) {
-  return (
-  <article className="appointment">
-    <Header time={props.time}/>
-    {props.interview ? <Show 
-    student={props.interview.student}
-    interviewer={props.interview.interviewer}
-    /> : <Empty />}
-  </article>
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE ="CREATE"
+  
+  const { mode, transition, back } = useVisualMode (
+    props.interview ? SHOW : EMPTY
   );
+
+
+   return (
+
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+       <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+       />
+      )} 
+      {mode === CREATE &&
+      <Form interviewers={[]} onCancel={back}/>}
+    </article>
+  )
 }
