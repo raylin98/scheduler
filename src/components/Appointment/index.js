d2fcb73d15392857e127993ade9d706053a58a48
Raymond Lin <raymond.lin.7690@gmail.com>
2023-07-27 16:18:37 -0700

    Adding a missing s for interviewers on line 35

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -32,7 +32,7 @@ export default function Appointment(props) {
        />
       )} 
       {mode === CREATE &&
-      <Form interviewers={props.interviewer} onCancel={back}/>}
+      <Form interviewers={props.interviewers} onCancel={back}/>}
     </article>
   )
 }
