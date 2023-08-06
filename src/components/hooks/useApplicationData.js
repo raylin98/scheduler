99e9e628186deebb93c141d66c071b96ae019873
Raymond Lin <raymond.lin.7690@gmail.com>
2023-08-05 17:46:29 -0700

    Updating axios get requests

--- src/components/hooks/useApplicationData.js
+++ src/components/hooks/useApplicationData.js
@@ -12,9 +12,9 @@ export default function useApplicationData() {
 
   useEffect(() => {
      Promise.all([
-      Promise.resolve(axios.get('http://localhost:8001/api/days')),
-      Promise.resolve(axios.get('http://localhost:8001/api/appointments')),
-      Promise.resolve(axios.get('http://localhost:8001/api/interviewers'))
+      Promise.resolve(axios.get('/api/days')),
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers'))
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     })
@@ -53,7 +53,7 @@ export default function useApplicationData() {
       [id]: appointment
     }
     return axios
-    .put(`http://localhost:8001/api/appointments/${id}`, {interview:interview})
+    .put(`/api/appointments/${id}`, {interview:interview})
     .then(res => {
       setState({...state, appointments, days})
       return res.status
@@ -82,7 +82,7 @@ export default function useApplicationData() {
     }
     
     return axios
-    .delete(`http://localhost:8001/api/appointments/${id}`, appointment)
+    .delete(`/api/appointments/${id}`, appointment)
     .then(res => {
       setState({...state, appointments, days})
       return res
