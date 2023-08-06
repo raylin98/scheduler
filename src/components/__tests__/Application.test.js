a2acd3a5985d0261d8557b598a3591929a88c8ad
Raymond Lin <raymond.lin.7690@gmail.com>
2023-08-05 17:46:59 -0700

    Adding mocks for Application testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import axios from "axios";
+
+import { render, cleanup, getByText, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    const {getByText} = render(<Application />);
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  })
 });
